c6abe2a3522446ae24cfecdfcd85823b807029ba
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2022-10-07 16:30:30 -0300

    req tres feito

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { coinApiAction } from '../redux/actions';
+import requestApi from '../services/coinsApi';
 
 class WalletForm extends Component {
   state = {
     expense: '',
-    // description: '',
-    // coin: '',
-    // paymentMethod: '',
-    // category: '',
+    description: '',
+    coin: 'USD',
+    paymentMethod: 'Dinheiro',
+    category: 'Alimentação',
   };
+
+  async componentDidMount() {
+    const { dispatch } = this.props;
+    const coin = await requestApi();
+    dispatch(coinApiAction(coin));
+  }
 
   inputChange = ({ target }) => {
     const { name, value } = target;
@@ -21,14 +30,15 @@ class WalletForm extends Component {
   render() {
     const {
       expense,
-      // description,
-      // coin,
-      // paymentMethod,
-      // category,
-      // coinArray,
+      description,
+      coin,
+      paymentMethod,
+      category,
     } = this.state;
 
-    // const { coins } = this.props;
+    const { coins } = this.props;
+    const arrayOfPayment = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+    const arrayOfCategory = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
 
     return (
       <main>
@@ -37,9 +47,11 @@ class WalletForm extends Component {
           <input
             type="text"
             id="expense"
-            name="exchange"
+            name="expense"
             value={ expense }
             onChange={ this.inputChange }
+            data-testid="value-input"
+            placeholder="Type a value"
           />
         </label>
 
@@ -48,9 +60,57 @@ class WalletForm extends Component {
           <select
             name="coin"
             id="coin"
+            value={ coin }
+            onChange={ this.inputChange }
+            data-testid="currency-input"
           >
-            <option> ola </option>
+            { coins.map((coinss) => (
+              <option key={ coinss }>{coinss}</option>
+            ))}
           </select>
+        </label>
+
+        <label htmlFor="payment">
+          Método de Pagamento
+          <select
+            name="paymentMethod"
+            id="paymentMethod"
+            value={ paymentMethod }
+            onChange={ this.inputChange }
+            data-testid="method-input"
+          >
+            { arrayOfPayment.map((pay) => (
+              <option key={ pay }>{pay}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="category">
+          Categoria
+          <select
+            name="category"
+            id="category"
+            value={ category }
+            onChange={ this.inputChange }
+            data-testid="tag-input"
+          >
+            { arrayOfCategory.map((cat) => (
+              <option key={ cat }>{cat}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="description">
+          Descrição
+          <input
+            type="text"
+            name="description"
+            id="description"
+            value={ description }
+            onChange={ this.inputChange }
+            placeholder="Type a Description"
+            data-testid="description-input"
+          />
         </label>
       </main>
     );
@@ -60,5 +120,10 @@ class WalletForm extends Component {
 const mapStateToProps = (state) => ({
   coins: state.wallet.currencies,
 });
+
+WalletForm.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  coins: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default connect(mapStateToProps)(WalletForm);
--- src/services/coinsApi.js
+++ src/services/coinsApi.js
@@ -1,8 +1,11 @@
-// const requestApi = async () => {
-//   const url = 'https://economia.awesomeapi.com.br/json/all';
-//   await fetch(url)
-//     .then((request) => request.json());
-//     .then(console.log);
-// };
+const requestApi = async () => {
+  const url = 'https://economia.awesomeapi.com.br/json/all';
+  const request = await fetch(url);
+  const data = await request.json();
 
-// export default requestApi;
+  const arraycoin = Object.keys(data);
+  arraycoin.splice(1, 1);
+  return arraycoin;
+};
+
+export default requestApi;
